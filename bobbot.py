db8e4cbbfae43eb050c32bf1d96579ace7cf39cb
Jayden Campbell <jaydencampbell590@gmail.com>
2024-03-21 18:22:33 -0400

    Added modifiable lines - changed command name

--- bobbot.py
+++ bobbot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import secret_key
+import random
 
 intents = discord.Intents.default()
 intents.message_content=True
@@ -8,7 +9,16 @@ intents.message_content=True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("Who goes there?!?")
+async def poke(ctx):
+    quotes = ["Citizen.",
+              "Move along.",
+              "Who goes there?!",
+              "Halt.",
+              "Traveler.",
+              "Keep your nose clean, citizen.",
+             "If you're traveling outside the city, you watch yourself, ya hear?",
+             "It's a peaceful day. Don't go and change that on my watch."]
+    quote_idx = random.randint(0, len(quotes) - 1)
+    await ctx.send(quotes[quote_idx])
 
 bot.run(secret_key.token)
